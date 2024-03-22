5e878a13f2d990f98c250c3dcbf77e9235b9cccc
XiaoZ218 <150356012+XiaoZ218@users.noreply.github.com>
2024-03-22 22:38:02 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
 
   HEXO_HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
   // 已知未修复bug, 在移动端开启true后会加载不出图片； 暂时建议设置为false。
-  HEXO_HOME_NAV_BACKGROUND_IMG_FIXED: true, // 首页背景图滚动时是否固定，true 则滚动时图片不懂动； false则随鼠标滚动 ;
+  HEXO_HOME_NAV_BACKGROUND_IMG_FIXED: false, // 首页背景图滚动时是否固定，true 则滚动时图片不懂动； false则随鼠标滚动 ;
   // 是否显示开始阅读按钮
   HEXO_SHOW_START_READING: true,
 
@@ -22,7 +22,7 @@ const CONFIG = {
   HEXO_POST_LIST_COVER_DEFAULT: false, // 封面为空时用站点背景做默认封面
   HEXO_POST_LIST_SUMMARY: true, // 文章摘要
   HEXO_POST_LIST_PREVIEW: true, // 读取文章预览
-  HEXO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
+  HEXO_POST_LIST_IMG_CROSSOVER: false, // 博客列表图片左右交错
 
   HEXO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
   HEXO_ARTICLE_COPYRIGHT: false, // 显示文章版权声明
